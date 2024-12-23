222885dde4eaf1faa53fcad0d9d8b317629c3389
codegreen <codegreen19s@gmail.com>
2024-12-23 14:23:10 +0600

    commented a problematic code

--- components/branch/students/_docs/DownloadListForExcel.tsx
+++ components/branch/students/_docs/DownloadListForExcel.tsx
@@ -50,35 +50,31 @@ export default function DownloadListForExcel({
   const studentData: StudentData[] = modifyData(studentInfo);
 
   const handleDownloadExcel = () => {
-    const worksheet = XLSX.utils.json_to_sheet(studentData);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-
-    // Calculate column widths based on the maximum length of content in each column
-    const maxLengths = Object.keys(studentData[0]).map((key) =>
-      Math.max(
-        ...studentData.map(
-          (row) => row[key as keyof StudentData].toString().length
-        ),
-        key.length
-      )
-    );
-
-    worksheet["!cols"] = maxLengths.map((len) => ({ width: len + 2 })); // Add padding for readability
-
-    const excelBuffer = XLSX.write(workbook, {
-      bookType: "xlsx",
-      type: "array",
-    });
-
-    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.setAttribute("download", "students.xlsx"); // Set the file name
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    // const worksheet = XLSX.utils.json_to_sheet(studentData);
+    // const workbook = XLSX.utils.book_new();
+    // XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    // // Calculate column widths based on the maximum length of content in each column
+    // const maxLengths = Object.keys(studentData[0]).map((key) =>
+    //   Math.max(
+    //     ...studentData.map(
+    //       (row) => row[key as keyof StudentData].toString().length
+    //     ),
+    //     key.length
+    //   )
+    // );
+    // worksheet["!cols"] = maxLengths.map((len) => ({ width: len + 2 })); // Add padding for readability
+    // const excelBuffer = XLSX.write(workbook, {
+    //   bookType: "xlsx",
+    //   type: "array",
+    // });
+    // const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+    // const url = URL.createObjectURL(blob);
+    // const link = document.createElement("a");
+    // link.href = url;
+    // link.setAttribute("download", "students.xlsx"); // Set the file name
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
   };
 
   return <div onClick={handleDownloadExcel}>{children}</div>;
